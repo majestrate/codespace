47f4be2d8c936e30cfd5ff0c6923faa62062638b
Jeff Becker <jeff@wikileaks2.org>
2023-12-18 12:40:26 -0500

    setup

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+# clone asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
+echo >> ~/.bashrc
+echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+
+# copy tool-versions
+cp -av tool-versions ~/.tool-versions
+
+# add plugins
+for plug in $(cat ~/.tool-versions | cut -d' ' -f1); do
+  asdf plugin add $plug
+done
+
+# install all plugins
+
 asdf install
